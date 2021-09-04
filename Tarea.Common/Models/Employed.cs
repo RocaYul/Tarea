b33da56f9cdc053de623b6fdb0ea17af1f147b02
Hermanas Roldan <yroldancarvajal@gmail.com>
2021-09-03 20:33:47 -0500

    Created entities an model of proyect

---
+++ Tarea.Common/Models/Employed.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tarea.Common.Models
+{
+    public class Employed
+    {
+        public int IdEmployed { get; set; }
+        public DateTime InputOutput { get; set; }
+        public int Type { get; set; }
+        public bool Consolidated { get; set; }
+    }
+}
---
+++ Tarea.Function/Entities/EmployedEntity.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+
+namespace Tarea.Function.Entities
+{
+    public class EmployedEntity : TableEntity
+    {
+        public EmployedEntity() { }
+        public int IdEmployed { get; set; }
+        public DateTime InputOutput { get; set; }
+        public int Type { get; set; }
+        public bool Consolidated { get; set; }
+    }
+}
